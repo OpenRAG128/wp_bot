a4d493629e4a3248da86aacebaf48ad32e2c971d
OpenRAG <openrag189@gmail.com>
2025-07-10 12:32:10 +0530

    Update wp_bot.py

--- wp_bot.py
+++ wp_bot.py
@@ -94,6 +94,10 @@ def whatsapp():
         )
         return str(resp)
 
+@app.route('/ping')
+def ping():
+    return "✅ I'm alive!"
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
